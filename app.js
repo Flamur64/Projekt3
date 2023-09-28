975b874b4900ab03b9af5c4b9752973cf1867322
Flamur Imeri <136818887+Flamur64@users.noreply.github.com>
2023-09-28 12:21:08 +0200

    verbesserungen

--- app.js
+++ app.js
@@ -1,54 +1,52 @@
-"use strict";
-// with npm installed
-let express = require("express");
-let bodyParser = require("body-parser");
-let GuestbookEntry = require("./src/blogEntry");
+let express = require("express")
+let bodyParser = require("body-parser")
+let GuestbookEntry = require("./src/blogEntry")
 
-let app = express();
+let app = express()
 
-// ejs nutzbar
-app.set("view engine", "ejs");
-app.set("views", "./views");
+app.use(express.static("./public"))
+app.use(express.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
-
-
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static("./public")); // makes the file "public" accecable
-
-let entries = [   // list for entrys ( see src)
-    new GuestbookEntry("BlogTitel", " HEEEELOOOOOOO WOOORLD!"),  // Constructer defined on Guestbook entry
-];
-
-
-
-//req ---> enthält informationen wie Code,parameter
-// res (response) --> Antwort an den browser
-
-
-//Antwort des servers bei /index (kern)
-app.get("/index", (req, res) => {           
-    res.render("index", {
-        entries: entries  // bblogentry.js
-    });      // deklariert durch <%= entries.length %>
-});
-
-
-// Daten vom Browser zum Server schicken
-app.post("/blog/new", (req,res) =>{
-let content = req.body.content; // 
-let title = req.body.title;  // bodyparser
-
-let newEntry = new GuestbookEntry(title, content);
-entries.push(newEntry);
-res.redirect("/index"); // browser wird auf index weitergeleitet
-
-});
-
-
-
-
-app.listen(5000, () => {
-    console.log("App wurde gestartet auf localhost:5000");
+app.get("/", (req, res) => {
+  res.send('<a href="/login">Zum Login</a>')
 })
 
+app.get("/login", function (req, res) {
+  res.sendFile("login.html", { root: "./" })
+})
 
+app.post("/login", function (req, res) {
+  const { benutzername, passwort } = req.body
+  if (benutzername === "gruppe3" && passwort === "12345") {
+    res.redirect("/index")
+  } else {
+    res.send("Anmeldung fehlgeschlagen.")
+  }
+})
+
+app.set("view engine", "ejs")
+app.set("views", "./views")
+
+let entries = [
+    new GuestbookEntry("BlogTitel", " HEEEELOOOOOOO WOOORLD!"),
+]
+
+app.get("/index", (req, res) => {           
+    res.render("index", {
+        entries: entries
+    })
+})
+
+app.post("/blog/new", (req,res) =>{
+let content = req.body.content
+let title = req.body.title
+
+let newEntry = new GuestbookEntry(title, content)
+entries.push(newEntry)
+res.redirect("/index")
+})
+
+app.listen(5000, () => {
+    console.log("App wurde gestartet auf localhost:5000")
+})
